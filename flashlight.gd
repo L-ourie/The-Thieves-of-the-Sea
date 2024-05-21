cca4279bf529a6d9de2f4fe49983b2817560efd5
TheSigmaCoder420 <smonoa@edu.emsb.qc.ca>
2024-05-21 14:59:38 -0400

    flash

--- flashlight.gd
+++ flashlight.gd
@@ -9,3 +9,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _input(event):
+	if event.is_action_pressed("Flashlight"):
+		$AnimationPlayer.play("moveflashlightf")
+		$AnimationPlayer.play("flaslight")
+	if event.is_action_pressed("Closeflashlight"):
+		$AnimationPlayer.play("moveflashlightb")
+		$AnimationPlayer.play("closeflashlight")
